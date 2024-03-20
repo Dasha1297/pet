348e4774ce3c36965d3c6904fc2950ff0bcd41b2
Darya Lukina <dashalukina1297@gmail.com>
2024-03-20 19:30:41 +0700

    add auth modal in navbar

---
+++ src/shared/UI/Modal/Modal.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Modal } from './Modal';
+
+export default {
+    title: 'shared/Modal',
+    component: Modal,
+    argTypes: {
+        isOpen: { control: 'boolean' },
+    },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Text',
+    isOpen: true,
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,24 +1,34 @@
-import { useTranslation } from 'react-i18next'
-import { classNames } from 'shared/lib/classNames/classNames'
-import { AppLink, AppLinkTheme } from 'shared/UI/AppLink/AppLink'
-import cls from './Nabbar.module.scss'
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ThemeButton } from 'shared/UI/Button/Button';
+import { Modal } from 'shared/UI/Modal/Modal';
+import cls from './Nabbar.module.scss';
 
 interface NavbarProps {
-    className?: string
+    className?: string;
 }
 export const Navbar = ({ className }: NavbarProps) => {
-    const { t } = useTranslation()
+    const { t } = useTranslation();
+
+    const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModalOpen((prev) => !prev);
+    }, []);
 
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink to="/" theme={AppLinkTheme.SECONDARY}>
-                    {t('главная')}
-                </AppLink>
-                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-                    {t('о сайте')}
-                </AppLink>
-            </div>
+            <Button theme={ThemeButton.CLEAR_INVERTED} onClick={onToggleModal}>
+                {t('Войти')}
+            </Button>
+            <Modal
+                isOpen={isAuthModalOpen}
+                onClose={onToggleModal}
+                // eslint-disable-next-line i18next/no-literal-string
+            >
+                fhhhhhh
+            </Modal>
         </div>
-    )
-}
+    );
+};
