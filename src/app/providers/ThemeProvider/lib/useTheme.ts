6865fd0a0fdf3d1ad38c029d49aa8f3edf9b6710
Darya Lukina <dashalukina1297@gmail.com>
2024-05-02 16:16:36 +0700

    update theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,21 +1,20 @@
-import { useContext } from "react";
-import { Theme, ThemeContext, LOCAL_STORAGE_THEME_KEY } from "./ThemeContext";
-
-
+import { useContext } from 'react';
+import { Theme, ThemeContext, LOCAL_STORAGE_THEME_KEY } from './ThemeContext';
 
 interface UseThemeResult {
-  toggleTheme: () => void;
-  theme: Theme
+    toggleTheme: () => void;
+    theme: Theme;
 }
 
-export function useThemeResult(): UseThemeResult{
-    const {theme, setTheme} = useContext(ThemeContext);
+export function useThemeResult(): UseThemeResult {
+    const { theme, setTheme } = useContext(ThemeContext);
 
-    const toggleTheme =()=>{
-        const newTheme = theme=== Theme.DARK ? Theme.LIGHT : Theme.DARK
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
         setTheme(newTheme);
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
-    }
+        document.body.className = newTheme;
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+    };
 
-    return {theme, toggleTheme}
+    return { theme, toggleTheme };
 }
